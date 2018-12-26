3510f9497b3767825ae05655153d912b51888471
andreasxp <andres.zhukov@gmail.com>
2018-12-26 05:22:56 +0300

    Added constexpr and noexcept to proxy_iterator Added as_functor

--- zhlib/zh/proxy_iterator.hpp
+++ zhlib/zh/proxy_iterator.hpp
@@ -1,50 +1,73 @@
 #pragma once
 #include <iterator>
+#include <memory>
 #include <type_traits>
+#include "type_traits/is_function_like.hpp"
+#include "as_functor.hpp"
 
 namespace zh {
 
 template <class BaseIterator, class FunctorType>
-class proxy_iterator : public BaseIterator, private FunctorType {
+class proxy_iterator : 
+	public BaseIterator, 
+	private FunctorType {
 public:
-	// Member types ============================================================
+	// Member types ------------------------------------------------------------
+	using iterator_type = BaseIterator;
+	using functor_type = FunctorType;
+
 	using value_type
 		= typename std::remove_reference_t<decltype(
-			std::declval<FunctorType>()(*std::declval<BaseIterator>()))>;
+			std::declval<functor_type>()(*std::declval<iterator_type>()))>;
 	using difference_type
-		= typename std::iterator_traits<BaseIterator>::difference_type;
+		= typename std::iterator_traits<iterator_type>::difference_type;
 	using pointer
 		= value_type * ;
 	using reference
 		= value_type & ;
 	using iterator_category
-		= typename std::iterator_traits<BaseIterator>::iterator_category;
+		= typename std::iterator_traits<iterator_type>::iterator_category;
 
-public:
-	using BaseIterator::BaseIterator;
-
-	explicit proxy_iterator(const BaseIterator& other) :
-		BaseIterator(other) {
+	// Member functions -------------------------------------------------------
+	explicit constexpr proxy_iterator(
+		const iterator_type& other = iterator_type(), 
+		const functor_type& functor = functor_type())
+		noexcept(
+			std::is_nothrow_copy_constructible_v<iterator_type> &&
+			std::is_nothrow_copy_constructible_v<functor_type>) :
+		iterator_type(other),
+		functor_type (functor) {
 	}
 
-	proxy_iterator(const BaseIterator& other, const FunctorType& functor) :
-		BaseIterator(other),
-		FunctorType(functor) {
+	explicit constexpr proxy_iterator(const functor_type& functor)
+		noexcept(proxy_iterator(iterator_type(), functor)) :
+		proxy_iterator(iterator_type(), functor) {
 	}
 
 	// Convert from related proxy iterators
-	// Example: proxy_iterator<iterator, ...> to proxy_iterator<const_iterator, ...>
-	template<class Iter, class = typename std::enable_if<std::is_convertible<Iter, BaseIterator>::value>::type>
-	proxy_iterator(const proxy_iterator<Iter, FunctorType>& other) :
-		BaseIterator(static_cast<const Iter&>(other)) {
+	// Example: proxy_iterator<iterator, ...> to 
+	//          proxy_iterator<const_iterator, ...>
+	template<class Iter, class = 
+		std::enable_if_t<std::is_constructible_v<iterator_type, const Iter&>>>
+	constexpr proxy_iterator(const proxy_iterator<Iter, functor_type>& other)
+		noexcept(
+			std::is_nothrow_constructible_v<iterator_type, const Iter&> &&
+			std::is_nothrow_default_constructible_v<functor_type>) :
+		iterator_type(static_cast<const Iter&>(other)) {
 	}
 
-	decltype(auto) operator->() const {
-		return &FunctorType::operator()(*static_cast<const BaseIterator&>(*this));
+	constexpr decltype(auto) operator->() const
+		noexcept(std::addressof(
+			functor_type::operator()(*static_cast<const iterator_type&>(*this)))) {
+		return std::addressof(
+			functor_type::operator()(*static_cast<const iterator_type&>(*this)));
 	}
 
-	decltype(auto) operator*() const {
-		return FunctorType::operator()(*static_cast<const BaseIterator&>(*this));
+	constexpr decltype(auto) operator*() const
+		noexcept(
+			functor_type::operator()(*static_cast<const iterator_type&>(*this))) {
+		return 
+			functor_type::operator()(*static_cast<const iterator_type&>(*this));
 	}
 };
 
--- zhlib/zh/type_traits.hpp
+++ zhlib/zh/type_traits.hpp
@@ -3,14 +3,16 @@
 #include "type_traits/enable_if_defined.hpp"
 #include "type_traits/defines_type.hpp"
 #include "type_traits/has_size.hpp"
+#include "type_traits/is_ratio.hpp"
+#include "type_traits/is_function_like.hpp"
+#include "type_traits/is_similar.hpp"
 
 #include "type_traits/is_range.hpp"
 #include "type_traits/is_bidirectional_range.hpp"
-
-#include "type_traits/is_ratio.hpp"
-#include "type_traits/is_similar.hpp"
 
 #include "type_traits/is_less_cv.hpp"
 #include "type_traits/is_more_cv.hpp"
 #include "type_traits/is_weaker_qualified.hpp"
 #include "type_traits/is_stronger_qualified.hpp"
+
+#include "type_traits/remove_all_pointers.hpp"
